29218a364fb210a1506043b450766f6d5f24421d
Neo <neo@orz.hk>
2016-05-21 18:46:52 +0800

    init ios

---
+++ ios/Medchain/Medchain/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Medchain
+//
+//  Created by Neo on 5/21/16.
+//  Copyright © 2016 padblish. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
